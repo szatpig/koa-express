b4b828eec81f547d00c0ab9e20f262bfb00aa165
ZCW\Administrator <1304485196@qq.com>
2018-11-01 18:13:54 +0800

    修改接口api地址

--- config/index.js
+++ config/index.js
@@ -4,7 +4,9 @@ const config = {
     port:3333,
     db: 'mongodb://127.0.0.1:27017/local', // 之所以两个只为了兼容旧版命名；请优先采用后者,
     secret:'1304485196@qq.com',
-    express_key:'cssdBokR6358'
+    express_key:'cssdBokR6358',
+    express_customer:'8AD9CE8D59EBA7E578FE1231C1157368',
+    kd_key:'3c6d2477-9250-4250-993b-e0914d9cf68c'
 };
 
 module.exports = config;
